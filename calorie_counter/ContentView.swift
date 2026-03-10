ed373cc0423c4b7646e365aa35d5fc6206916b9b
Roy Stanfield <roystanfield@gmail.com>
2026-03-10 12:26:06 -0400

    Initial Commit

---
+++ calorie_counter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  calorie_counter
+//
+//  Created by Roy Stanfield on 3/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ calorie_counter/calorie_counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  calorie_counterApp.swift
+//  calorie_counter
+//
+//  Created by Roy Stanfield on 3/10/26.
+//
+
+import SwiftUI
+
+@main
+struct calorie_counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
